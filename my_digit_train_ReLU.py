Fixed predict function and started Softmax.py

my_digit_train_ReLU.py
@@ -39,8 +39,10 @@ def forward_propagate(input_vector):
     return output_vector
     
 def predict(input_vector):
+    #accepts rows as input (easier to format)
     input_vector = np.asmatrix(input_vector, dtype=float).T
-    return forward_propagate(input_vector)
+    #returns output as rows
+    return forward_propagate(input_vector).T
     
 def backwards_propagate(training_input, expected_output, learn_rate = 0.1):
     global hidden_vector, output_vector, W2, W1, b2, b1
@@ -75,7 +77,7 @@ def train_network(training_input, training_output, batch_size = 100, num_of_iter
     training_output = np.asmatrix(training_output, dtype=float)
 
     for i in range(num_of_iterations): #Also possible to take random batches so it doesn't "Overfit"
-        i = int(i % np.shape(training_input)[1] / batch_size)
+        i = int(i % np.shape(training_input)[0] / batch_size)
         batch_input = training_input[i * batch_size : (i+1) * batch_size : ].T
         batch_output = training_output[i * batch_size : (i+1) * batch_size : ].T
         backwards_propagate(batch_input , batch_output, learn_rate)
@@ -83,7 +85,7 @@ def train_network(training_input, training_output, batch_size = 100, num_of_iter
 #===================== Utilities =============================
 
 def one_hot_predict(input):
-    probabilities = predict(input).T
+    probabilities = predict(input)
     return probabilities.argmax(axis=1)
     
 def test_accuracy(input_layer, output_layer):
@@ -117,7 +119,7 @@ training_images = training[3]
 training_labels = training[4]
 training_labels = np.squeeze(training_labels)
 # ======================= Parameters ==========================
-N = 100 #Number of tests per digit
+N = 10 #Number of tests per digit
 
 # ======================= Create A ============================
 A_all = np.zeros((10*N, 28*28))

my_neural_net_ReLu_SoftMax.py
@@ -4,8 +4,8 @@ import numpy as np
 #Memory Setup
 
 input_layer_size = 2
-hidden_layer_size = 2 #5
-output_layer_size = 1 #10
+hidden_layer_size = 3
+output_layer_size = 2
 
 W1 = np.random.rand(hidden_layer_size, input_layer_size)
 W2 = np.random.rand(output_layer_size, hidden_layer_size)
@@ -21,6 +21,11 @@ def relu_deriv(s, EPS = 0.01):
 def relu(s, EPS = 0.01):
     return (np.multiply((s > 0), s * (1 - EPS)) + EPS*s)
 
+def softmax(v):
+    v = v - v.max(0) #shift by largest value
+    new_v = np.exp(v)
+    return  new_v / new_v.sum(0)
+
 def normalize(A, EPS = 0.1):
     m = np.abs(A).max()
     if(m < EPS):
@@ -31,14 +36,16 @@ def forward_propagate(input_vector):
     global hidden_vector, output_vector
     #accepts columns as input
     hidden_vector = relu(W1 @ input_vector + b1)
-    output_vector = W2 @ hidden_vector + b2
+    output_vector = softmax(W2 @ hidden_vector + b2)
     
     #returns output as columns
     return output_vector
     
 def predict(input_vector):
+    #accepts rows as input (easier to format)
     input_vector = np.asmatrix(input_vector, dtype=float).T
-    return forward_propagate(input_vector)
+    #returns output as rows
+    return forward_propagate(input_vector).T
     
 def backwards_propagate(training_input, expected_output, learn_rate = 0.1):
     global hidden_vector, output_vector, W2, W1, b2, b1
@@ -47,8 +54,9 @@ def backwards_propagate(training_input, expected_output, learn_rate = 0.1):
     
     num_of_inputs = np.shape(training_input)[1]
     
-    delta_output = output_vector - expected_output
-    delta_W2 = delta_output @ np.transpose(hidden_vector) / num_of_inputs
+    delta_output = (output_vector - 1) #delta according to cross-entropy and softmax gradient
+    
+    delta_W2 = delta_output @ hidden_vector.T
     delta_b2 = np.sum(delta_output) / num_of_inputs
     
     delta_hidden_layer = np.multiply(W2.T @ delta_output,  relu_deriv(hidden_vector))
@@ -61,7 +69,6 @@ def backwards_propagate(training_input, expected_output, learn_rate = 0.1):
 
 def update_params(delta_W2, delta_b2, delta_W1, delta_b1, learn_rate):
     global W1, W2, b1, b2
-    
     W2 -= normalize(delta_W2) * learn_rate
     b2 -= normalize(delta_b2) * learn_rate
     W1 -= normalize(delta_W1) * learn_rate
@@ -80,12 +87,10 @@ def train_network(training_input, training_output, batch_size = 100, num_of_iter
 
 #====================== Data ==============================
 
-input_layer = np.array([[2,9], [3,1], [4,1]])
-output_layer = np.array([[92], [28], [69]])
+input_layer = np.array([[2,9], [3,4], [4,1]])
+output_layer = np.array([[0, 1], [0,1], [1,0]])
 
-output_layer = output_layer/100 # maximum test score is 100
-
-train_network(input_layer, output_layer, 1, 10000, 0.5)
+train_network(input_layer, output_layer, 1, 100, 0.5)
 
 print("Weights and Bias:\nW1:", W1, "\nW2:", W2, "\nb1:", b1, "\nb2:", b2)
-print("Example tests:", predict([[2,9], [3,1], [4,1]]))
+print("Example tests:", predict([[2,9], [3,4], [4,1]]))