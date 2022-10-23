ef57f608bd19a3951f6fa54918af43107f8b6fc0
Lil' Gitty <ripley37@protonmail.com>
2022-10-23 10:16:38 +0300

    Updated find_digit with the 10 classifiers

--- digit_classification/find_digit.py
+++ digit_classification/find_digit.py
@@ -48,15 +48,14 @@ print("Data Loaded")
 
 image = Image.open("f.png")
 image = image.convert("L") #Black And White
-#image = np.asarray(image) / 255
 
 #digits_found = []
 
 test_window = np.asarray(image.resize((digit_width, digit_height))) / 255
 probabilities = predict_probabilities(digit_nets, np.reshape(test_window, (digit_width*digit_height)))
 predicted_digit = np.argmax(probabilities)
-plt.imshow(test_window, cmap='gray')
 possible_digits = np.array(np.where(probabilities > 0.9)[1])
+plt.imshow(test_window, cmap='gray')
 plt.title('prediction: ' + str(np.argmax(probabilities)) + " probabilities:" + str(probabilities) + " possible digits: " + np.array_str(possible_digits))
 #plt.title('Looking for Digit. prediction: ' + str(predicted_digit) + " confidence:" + str(probabilities[predicted_digit]))
 plt.axis('image')
@@ -66,17 +65,19 @@ plt.waitforbuttonpress()
 plt.clf()
 
 
-# for k in range(min(image.shape) // min(digit_width, digit_height) - 5, 0, -1):
+# image = np.asarray(image) / 255
+# for k in range(min(image.shape) // min(digit_width, digit_height), 0, -1):
     # for x in range(0, image.shape[0] - k*digit_width, 10):
         # for y in range(0, image.shape[1] - k*digit_height, 10):
             # test_window = image[x : x + k*digit_width, y : y + k*digit_height]
             # test_window = np.asarray(Image.fromarray(test_window).resize((digit_width, digit_height))) #Classic Python Moment
             
             # #prediction
-            # probabilities = classify_digit_net.predict(np.reshape(test_window, (digit_width*digit_height))).T
+            # probabilities = predict_probabilities(digit_nets, np.reshape(test_window, (digit_width*digit_height)))
             # predicted_digit = np.argmax(probabilities)
+            # possible_digits = np.array(np.where(probabilities > 0.9)[1])
             # plt.imshow(test_window, cmap='gray')
-            # plt.title('Looking for Digit. prediction: ' + str(predicted_digit) + " confidence:" + str(probabilities[predicted_digit]))
+            # plt.title('Looking for digit. prediction: ' + str(np.argmax(probabilities)) + " probabilities:" + str(probabilities) + " possible digits: " + np.array_str(possible_digits))
             # plt.axis('image')
             # plt.axis('off')
             # plt.show(block=False)
